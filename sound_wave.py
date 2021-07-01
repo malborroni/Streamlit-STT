b02921fafaf061fc5d5225bca7548a8b0a9c7e60
Alessandro Borroni <45471380+malborroni@users.noreply.github.com>
2021-07-01 15:09:13 +0200

    Update sound_wave.py

--- sound_wave.py
+++ sound_wave.py
@@ -84,7 +84,8 @@ with play:
         st.write('\n\n\n')
         #st.image(white_img, width = 25)
         #st.write("« " + text_audio + " »")
-        st.write("« " + "Egregio signor le ricordo che oggi è il ventidue duemilaventuno e che la sto chiamando da Tunisi per conto con sede legale in per proporle una nuova offerta di energia elettrica nel mercato libero chi lei ha accettato la chiamata provvedendo a Tunisi La informo che Lei per la conclusione del contratto di fornitura energia elettrica ha il diritto di scegliere di accettare l'offerta sul mercato libero dopo aver ricevuto la nostra proposta contrattuale in forma scritta ed averla accettata per iscritto Intende rinunciare al diritto di concludere il contratto in forma scritta mi conferma Sì" + " »")
+        st.write("« " + "Egregio signor le ricordo che oggi è il ventidue duemilaventuno e che la sto chiamando da Tunisi per conto con sede legale in per proporle una nuova offerta di energia elettrica nel mercato libero chi lei ha accettato la chiamata provvedendo a Tunisi La informo che Lei per la conclusione del contratto di fornitura energia elettrica ha il diritto di scegliere di accettare l'offerta sul mercato libero dopo aver ricevuto la nostra proposta contrattuale in forma scritta ed averla accettata per iscritto Intende rinunciare al diritto di concludere il contratto in forma scritta mi conferma" + " »")
+        st.write("« " + "Sì" + " »")
 
 
 ######################### AUDIO #########################
