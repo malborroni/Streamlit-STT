30980eceff97d4b22e87476a7eebe7ed1abbfa68
Alessandro Borroni <45471380+malborroni@users.noreply.github.com>
2021-11-04 18:47:26 +0100

    Update sound_wave.py

--- sound_wave.py
+++ sound_wave.py
@@ -305,6 +305,8 @@ with sound:
             kpi2_col.markdown('<div style="text-align:left"><p class="small-font">&#8226; <b> Customer answer(s): </b></p></div>', unsafe_allow_html=True)
             kpi2_col.write('&emsp;8 sec.')
             
+    but1, but2, but3, but4, but5 = st.beta_columns(5)
+            
     if (but3.button("RESULT")):
         
         if example == "Q-1703186_VO.mp3":
